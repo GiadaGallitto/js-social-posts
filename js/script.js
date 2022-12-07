7ba337101b904ef9f3c8af0fb273fd93ed634e4a
GiadaGallitto <gallitto.giada@gmail.com>
2022-12-07 16:47:21 +0100

    Add elements with inner HTML

--- js/script.js
+++ js/script.js
@@ -58,38 +58,43 @@ const posts = [
 
 let divElement = document.getElementById("container")
 
+for (let i = 0; i < posts.length; i++){
 
+    const onePost = posts[i];
 
+    const authorInfos = onePost.author;
 
+    divElement.innerHTML += 
+    `        <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${authorInfos.image} " alt=" ${authorInfos.name} ">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${authorInfos.name}</div>
+                <div class="post-meta__time">${onePost.created} </div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${onePost.content} </div>
+    <div class="post__image">
+        <img src="${onePost.media} " alt="${onePost.media}">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${onePost.likes} </b> persone
+            </div>
+        </div> 
+    </div>            
+</div>`
 
-function createNewPost(infoImage){
-    const post = document.createElement("div");
-    post.classList.add("post");
-
-    const postHeader = document.createElement("div");
-    postHeader.classList.add("post__header");
-
-    const postMeta = document.createElement("div");
-    postMeta.classList.add("post-meta");
-
-    let metaIcon = document.createElement("div");
-    metaIcon.classList.add("post-meta__icon")
-
-    let imgIcon = document.createElement("img");
-    imgIcon.src = infoImage;
-
-    metaIcon.append(imgIcon);
-
-    let metaData = document.createElement("div");
-    metaData.classList.add("post-meta__data");
-
-    let metaAuthor = document.createElement("div");
-    metaAuthor.classList.add("post-meta__author");
-    metaAuthor.innerHTML = infoAuthor
-
-    let metaTime = document.createElement("div");
-    metaTime.classList.add("post-meta__time");
-    metaAuthor.innerHTML = infoAuthor
-
-    metaData.append(metaAuthor, metaTime)
 }
+
